7ba8d92b1614d9ea0e329571f992a0a8ddd9dac1
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-03-27 18:54:47 -0600

    created html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+const router = require("express").Router();
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+module.exports = router;
